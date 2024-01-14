35d624723565c92b0b8b857f1c208cd26e16c3b6
galigloria <85185233+galigloria@users.noreply.github.com>
2024-01-14 20:16:02 +0100

    Bejelentkezés Konstruktora , Bejelentkezés gomb ,Kilépés gomb, Felhasználó hitelesítés
    Az Alábbi kódsorokban lekezeltem a Bejelentkezés felület Konstrukrotár itt meghívtam a Bejeletkezés Panel-t , mint ablakot és beállítottam annak méretét. 
    Lekezeltem a "Bejelentkezés" gombot, amelyhez használtam egy user változót ami Felhasználó osztály típusú, amely osztályban a felhasználó adatait vannak megadva. Ha a user nem null, akkor megtörténik a bejelentkezés, ellenkező esetben hibaüzenetet kapunk a sikertelen bejelentkezésről.
    Meghívtam a kilépés gomb metódusát is. 
    Felhasználó hitelesítése adatbázison keresztül történik meg, a függvényben csatlakozunk az adatbázishoz, majd megkeressük a megegyező felhasználó nevet, illetve jelszót és visszatérünk a helyes felhasználóval. Ellenőrizni kell, hogy a Felhasználó osztályában megadott adatok megegyeznek az adatbázis mezőivel is.

--- BejelentkezesForm.java
+++ BejelentkezesForm.java
@@ -10,7 +10,73 @@ public class BejelentkezesForm extends JDialog{
     private JButton bt_bejelentkezes;
     private JButton bt_kilepes;
     private JPanel BejelentkezesPanel;
+    public BejelentkezesForm(JFrame parent) {
+        super(parent);
+        setTitle("Bejelentkezés");
+        setContentPane(BejelentkezesPanel);
+        setMinimumSize(new Dimension(700, 600));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
+        bt_bejelentkezes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = tf_email.getText();
+                String jelszo = String.valueOf(pf_jelszo.getPassword());
+                user = felhasznaloHitelesitese(email, jelszo);
+                if (user != null) {
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(BejelentkezesForm.this,
+                            "Helytelen e-mail vagy jelszó!",
+                            "Próbálja meg újra!",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+        });
+        bt_kilepes.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        setVisible(true);
+    }
+        public Felhasznalo user;
+        private Felhasznalo felhasznaloHitelesitese(String email, String jelszo) {
+            Felhasznalo user=null;
+            final String DB_URL="jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
+            final String USERNAME="root";
+            final String PASSWORD="";
+            try {
+                Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+                Statement statement=conn.createStatement();
+                String sql="SELECT*FROM felhasznalo WHERE email=? AND jelszo=?";
+                PreparedStatement preparedStatement=conn.prepareStatement(sql);
+                preparedStatement.setString(1,email);
+                preparedStatement.setString(2,jelszo);
+                ResultSet resultSet= preparedStatement.executeQuery();
+                if(resultSet.next())
+                {
+                    user=new Felhasznalo();
+                    user.nev=resultSet.getString("nev");
+                    user.email=resultSet.getString("email");
+                    user.telefonszam=resultSet.getString("telefonszam");
+                    user.lakcim=resultSet.getString("lakcim");
+                    user.jelszo=resultSet.getString("jelszo");
+                }
+                statement.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return user;
+
+        }
+
+    }
 }
 
 
