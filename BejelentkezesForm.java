3fa288ac6c69543584fcfdce3082aa6e08575137
galigloria <85185233+galigloria@users.noreply.github.com>
2024-01-14 20:01:44 +0100

    Bejelentkezés Felület Funkciói
    Megadtam a funkciókat, gombokat, szövegdobozokat, amelyek a Bejelentkezés felületén majd láthatóak lesznek és amelyeket a program elkészítése során felhasználok.

---
+++ BejelentkezesForm.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class BejelentkezesForm extends JDialog{
+    private JTextField tf_email;
+    private JPasswordField pf_jelszo;
+    private JButton bt_bejelentkezes;
+    private JButton bt_kilepes;
+    private JPanel BejelentkezesPanel;
+
+}
+
+
