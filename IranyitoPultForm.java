28756f678d260a5e033e91fce4959b3e4f55bf79
galigloria <85185233+galigloria@users.noreply.github.com>
2024-01-15 20:05:11 +0100

    Irányitópult Funkciói

---
+++ IranyitoPultForm.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class IranyitoPultForm  extends JFrame{
+    private JPanel IranyitoPultPanel;
+    private JLabel lb_admin;
+    private JButton bt_regisztracio;
+
+}
